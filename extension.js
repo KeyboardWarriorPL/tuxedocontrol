83c8220aa10e31af311f736865cd3f55640f9914
GummyBearStudio <keyboardwarriror@gmail.com>
2019-02-02 10:25:47 +0100

    Init of tuxedo Control panel for backlight

---
+++ extension.js
@@ -0,0 +1,225 @@
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Soup = imports.gi.Soup;
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
+const Clutter = imports.gi.Clutter;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const Util = imports.misc.util;
+
+const Gettext = imports.gettext.domain('gnome-shell-extension-tuxedocontrol');
+const _ = Gettext.gettext;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Config = imports.misc.config;
+const Convenience = Me.imports.convenience;
+
+const FOREX_SETTINGS_SCHEMA = 'org.gnome.shell.extensions.tuxedocontrol';
+const FOREX_PAIR_CURRENT = 'pair-current';
+const FOREX_REFRESH_INTERVAL = 'refresh-interval';
+const FOREX_PRICE_IN_PANEL = 'price-in-panel';
+const FOREX_ONLINE_STATUS = 'online-status';
+
+const QUOTES_URL = 'http://quotes.instaforex.com/get_quotes.php';
+const UP_POINTING = String.fromCharCode(9650);
+const DOWN_POINTING = String.fromCharCode(9660);
+const SERVER_TIME_GMT_DIFF = 10800;
+
+let _httpSession;
+
+const TuxedoCtl = new Lang.Class({
+    Name: 'TuxedoCtl',
+    Extends: PanelMenu.Button,
+
+    _init: function() {
+        this.parent(0.0, "Tuxedo Control", false);
+        this._loadConfig();
+        this._online_status = this._onlineStatusConf;
+        this.buttonText = new St.Label({
+            text: "TUX",
+            //icon_name: "keyboard",
+            //style_class: "input-keyboard",
+            y_align: Clutter.ActorAlign.CENTER
+        });
+        this.actor.add_actor(this.buttonText);
+        this._buildMenu();
+        this._refresh();
+    },
+
+    _buildMenu: function() {
+        this.menu.removeAll();
+        this.symbol = this._createMenuItem(_("Symbol"));
+        this.ask = this._createMenuItem(_("Ask"));
+        this.bid = this._createMenuItem(_("Bid"));
+        this.change = this._createMenuItem(_("Change"));
+
+        this.lasttime = new St.Label({
+            text: _("...")
+        });
+        let item = new PopupMenu.PopupBaseMenuItem({
+            reactive: false
+        });
+        item.actor.add(this.lasttime);
+        this.menu.addMenuItem(item);
+
+        let separator = new PopupMenu.PopupSeparatorMenuItem();
+        this.menu.addMenuItem(separator);
+
+        item = new PopupMenu.PopupMenuItem(_("Reload"));
+        item.connect('activate', Lang.bind(this, function() {
+            this._online_status = true;
+            this._refresh();
+        }));
+        this.menu.addMenuItem(item);
+
+        item = new PopupMenu.PopupMenuItem(_("Settings"));
+        item.connect('activate', Lang.bind(this, this._onPreferencesActivate));
+        this.menu.addMenuItem(item);
+
+        item = new PopupMenu.PopupMenuItem(_("Offline / Online"));
+        item.connect('activate', Lang.bind(this, this._setOffline));
+        this.menu.addMenuItem(item);
+    },
+
+    _createMenuItem: function(text) {
+        let label_right = new St.Label({
+            text: text
+        });
+        let label_left = new St.Label({
+            text: _("...")
+        });
+        let item = new PopupMenu.PopupBaseMenuItem({
+            reactive: false
+        });
+        item.actor.add(label_left, {
+            expand: true
+        });
+        item.actor.add(label_right);
+        this.menu.addMenuItem(item)
+        return label_left;
+    },
+
+    _loadData: function() {
+        this._refreshUI();
+    },
+
+    _refresh: function() {
+        this._loadData(this._refreshUI);
+        this._removeTimeout();
+        this._timeout = Mainloop.timeout_add_seconds(this._refreshInterval, Lang.bind(this, this._refresh));
+        return true;
+    },
+
+    _setOffline: function() {
+        if (this._online_status == true) {
+            this._removeTimeout();
+            this.buttonText.set_text(_("Offline"));
+            this._online_status = false;
+        } else {
+            this._online_status = true;
+            this._refresh();
+        }
+    },
+
+    _onPreferencesActivate: function() {
+        Util.spawn(["gnome-shell-extension-prefs", "tuxedocontrol@gbs"]);
+        return 0;
+    },
+
+    _loadConfig: function() {
+        this._settings = Convenience.getSettings(FOREX_SETTINGS_SCHEMA);
+        this._settingsC = this._settings.connect("changed", Lang.bind(this, function() {
+            this._refresh();
+        }));
+    },
+
+    _refreshUI: function(data) {
+        /*this.symbol.set_text(data.symbol);
+        this.ask.set_text(data.ask.toString());
+        this.bid.set_text(data.bid.toString());
+        this.change.set_text(data.change.toString());
+        let date = new Date((data.lasttime - SERVER_TIME_GMT_DIFF) * 1000);
+        this.lasttime.set_text(date.toLocaleString());
+
+        let txt;
+        if (this._priceInPanel == _("Ask"))
+            txt = this.change.text + ' ' + this.ask.text;
+        else
+            txt = this.change.text + ' ' + this.bid.text;*/
+
+        //this.buttonText.set_text(txt);
+    },
+
+    get _currentPair() {
+        if (!this._settings)
+            this._loadConfig();
+        return this._settings.get_string(FOREX_PAIR_CURRENT);
+    },
+
+    get _refreshInterval() {
+        if (!this._settings)
+            this._loadConfig();
+        return this._settings.get_int(FOREX_REFRESH_INTERVAL);
+    },
+
+    get _priceInPanel() {
+        if (!this._settings)
+            this._loadConfig();
+        return this._settings.get_string(FOREX_PRICE_IN_PANEL);
+    },
+
+    get _onlineStatusConf() {
+        if (!this._settings)
+            this._loadConfig();
+        return this._settings.get_boolean(FOREX_ONLINE_STATUS);
+    },
+
+    set _onlineStatusConf(v) {
+        if (!this._settings)
+            this._loadConfig();
+        this._settings.set_boolean(FOREX_ONLINE_STATUS, v);
+    },
+
+    _removeTimeout: function() {
+        if (this._timeout) {
+            Mainloop.source_remove(this._timeout);
+            this._timeout = null;
+        }
+    },
+
+    stop: function() {
+        if (_httpSession !== undefined)
+            _httpSession.abort();
+        _httpSession = undefined;
+
+        if (this._timeout)
+            Mainloop.source_remove(this._timeout);
+        this._timeout = undefined;
+
+        this._onlineStatusConf = this._online_status;
+
+        if (this._settingsC) {
+            this._settings.disconnect(this._settingsC);
+            this._settingsC = undefined;
+        }
+        this.menu.removeAll();
+    }
+});
+
+let tuxedoMenu;
+
+function init() {
+    Convenience.initTranslations('gnome-shell-extension-tuxedocontrol');
+}
+
+function enable() {
+    tuxedoMenu = new TuxedoCtl;
+    Main.panel.addToStatusArea('tuxedo-control', tuxedoMenu);
+}
+
+function disable() {
+    tuxedoMenu.stop();
+    tuxedoMenu.destroy();
+}
---
+++ prefs.js
@@ -0,0 +1,177 @@
+const GLib = imports.gi.GLib;
+const GObject = imports.gi.GObject;
+const Gio = imports.gi.Gio;
+const Gtk = imports.gi.Gtk;
+
+const Gettext = imports.gettext.domain('gnome-shell-extension-tuxedocontrol');
+const _ = Gettext.gettext;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
+const Lang = imports.lang;
+
+const FOREX_SETTINGS_SCHEMA = 'org.gnome.shell.extensions.tuxedocontrol';
+const FOREX_PAIR_CURRENT = 'pair-current';
+const FOREX_REFRESH_INTERVAL = 'refresh-interval';
+const FOREX_PRICE_IN_PANEL = 'price-in-panel';
+
+const SYMBOLS = [
+    'EURUSD',
+    'GBPUSD',
+    'USDJPY',
+    'USDCHF',
+    'USDCAD',
+    'EURJPY',
+    'EURCHF',
+    'GBPJPY',
+    'GBPCHF',
+    'NZDUSD',
+    'AUDUSD',
+    'EURCAD',
+    'EURAUD',
+    'EURGBP',
+    'GOLD'
+];
+
+function init() {
+    Convenience.initTranslations('gnome-shell-extension-tuxedocontrol');
+}
+
+const TuxedoPrefsWidget = new GObject.Class({
+    Name: 'TuxedoControlExtension.Prefs.Widget',
+    GTypeName: 'TuxedoControlExtensionPrefsWidget',
+    Extends: Gtk.Grid,
+
+    _init: function(params) {
+        this._loadConfig();
+        this.parent(params);
+        this.margin = 12;
+        this.row_spacing = this.column_spacing = 6;
+        this.set_orientation(Gtk.Orientation.VERTICAL);
+
+        let hbox = new Gtk.HBox();
+        let vbox = new Gtk.VBox();
+        let label = new Gtk.Label({
+            label: _("Price in panel"),
+            use_markup: true,
+            halign: Gtk.Align.START
+        });        
+        let radio = new Gtk.RadioButton();
+        let button = Gtk.RadioButton.new_with_label_from_widget(radio, _("Ask"), {
+            halign: Gtk.Align.CENTER
+        });
+        button.connect("toggled", Lang.bind(this, function() {
+            this._priceInPanel = _("Ask");
+        }));
+        if (button.label == this._priceInPanel)
+            button.set_active(true);
+        vbox.add(button);
+        button = Gtk.RadioButton.new_with_label_from_widget(radio, _("Bid"), {
+            halign: Gtk.Align.CENTER
+        });
+        button.connect("toggled", Lang.bind(this, function() {
+            this._priceInPanel = _("Bid");
+        }));
+        if (button.label == this._priceInPanel)
+            button.set_active(true);
+        vbox.add(button);
+        hbox.pack_start(label, false, false, 100);
+        hbox.pack_end(vbox, false, false, 100);
+        this.add(hbox);
+
+        hbox = new Gtk.HBox();
+        label = new Gtk.Label({
+            label: _("Time Update"),
+            hexpand: true,
+            halign: Gtk.Align.CENTER
+        });
+        let ad = new Gtk.Adjustment({
+            lower: 1.0,
+            step_increment: 1.0,
+            upper: 360.0,
+            value: 1.0
+        });
+        let spinButton = new Gtk.SpinButton({
+            adjustment: ad,
+            digits: 0,
+            xalign: 1,
+            halign: Gtk.Align.CENTER
+        });
+        spinButton.set_value(this._refreshInterval);
+        spinButton.connect("value_changed", Lang.bind(this, function() {
+            this._refreshInterval = spinButton.value;
+        }));
+        hbox.pack_start(label, false, false, 100);
+        hbox.pack_end(spinButton, false, false, 100);
+        this.add(hbox);
+        
+        hbox = new Gtk.HBox();
+        label = new Gtk.Label({
+            label: _("Symbols"),
+            use_markup: true,
+            halign: Gtk.Align.START
+        });
+        this._comboBox = new Gtk.ComboBoxText();
+        for (let i = 0; i < SYMBOLS.length; i++) {
+            this._comboBox.append_text(SYMBOLS[i]);
+            if (SYMBOLS[i] == this._currentPair)
+                this._comboBox.set_active(i);
+        }
+        this._comboBox.connect('changed', Lang.bind(this, this._onComboChanged));
+        hbox.pack_start(label, false, false, 100);
+        hbox.pack_end(this._comboBox, false, false, 100);
+        this.add(hbox);
+    },
+
+    _onComboChanged: function() {
+        let activeItem = this._comboBox.get_active();
+        this._currentPair = SYMBOLS[activeItem];
+    },
+
+    _loadConfig: function() {
+        this._settings = Convenience.getSettings(FOREX_SETTINGS_SCHEMA);
+    },
+
+    get _currentPair() {
+        if (!this._settings)
+            this._loadConfig();
+        return this._settings.get_string(FOREX_PAIR_CURRENT);
+    },
+
+    set _currentPair(v) {
+        if (!this._settings)
+            this._loadConfig();
+        this._settings.set_string(FOREX_PAIR_CURRENT, v);
+    },
+
+    get _refreshInterval() {
+        if (!this._settings)
+            this._loadConfig();
+        return this._settings.get_int(FOREX_REFRESH_INTERVAL);
+    },
+
+    set _refreshInterval(v) {
+        if (!this._settings)
+            this._loadConfig();
+        this._settings.set_int(FOREX_REFRESH_INTERVAL, v);
+    },
+
+    get _priceInPanel() {
+        if (!this._settings)
+            this._loadConfig();
+        return this._settings.get_string(FOREX_PRICE_IN_PANEL);
+    },
+
+    set _priceInPanel(v) {
+        if (!this._settings)
+            this._loadConfig();
+        this._settings.set_string(FOREX_PRICE_IN_PANEL, v);
+    },
+});
+
+function buildPrefsWidget() {
+    let widget = new TuxedoPrefsWidget();
+    widget.show_all();
+    return widget;
+}
